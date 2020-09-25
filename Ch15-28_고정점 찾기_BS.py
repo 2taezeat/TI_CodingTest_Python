5989fa98640dcbcdeb12a1d8e4a8374ddaa36bc0
2taezeat <59787852+2taezeat@users.noreply.github.com>
2020-09-25 14:06:49 +0900

    200925 / ramupgrade / BS

--- Ch15-28_고정점 찾기_BS.py
+++ Ch15-28_고정점 찾기_BS.py
@@ -1,14 +1,21 @@
 import bisect
-
-n, x = map(int,input().split())
+n = int(input())
 l1 = list(map(int,input().split()))
-l1.sort()
 
-lower = bisect.bisect_left(l1,x)
-upper = bisect.bisect_right(l1,x)
+def index1(a, x):
+    'Locate the leftmost value exactly equal to x'
+    i = bisect.bisect_left(a, x)
+    return (i < len(a) and a[i] == i)
 
-result = upper - lower
-if result == 0:
-    print(-1)
-else:
-    print(result)
+a = -1
+for i in range(n):
+    x = l1[i]
+    if x < 0:
+        continue
+
+    p = index1(l1,x)
+    if p == True:
+        a = x
+        break
+
+print(a)
