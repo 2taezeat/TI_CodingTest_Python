01157550f6d59339061d9c2d21d2f2f8f8bddc22
2taezeat <59787852+2taezeat@users.noreply.github.com>
2021-03-19 19:32:47 +0900

    210319_greedy

--- Ch3-4_1이 될 때 까지_greedy.py
+++ Ch3-4_1이 될 때 까지_greedy.py
@@ -1,13 +1,11 @@
 n , k = map(int,input().split())
 result = 0
-    
 
 while True:
     
     target = (n//k) * k
     result = result + (n - target)
     n = target
-
     
     if n < k:
         break
