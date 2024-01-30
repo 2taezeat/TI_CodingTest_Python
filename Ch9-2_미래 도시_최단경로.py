8ac7a3cd56dd9bd0a02129366d1445f3ff893c7f
2taezeat <xorudfl772@naver.com>
2024-01-30 23:04:34 +0900

    test commit 240130_2

--- Ch9-2_미래 도시_최단경로.py
+++ Ch9-2_미래 도시_최단경로.py
@@ -1,28 +1,25 @@
 INF = int(1e9)
-n, m = map(int,input().split())
-graph = [ [INF] * (n+1) for _ in range(n+1)]
+n, m = map(int, input().split())
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
 print(graph)
 
 # 자기 자신에서 자기 자신으로 가는 비용은 0으로 초기화
-for a in range(1,n+1):
-    for b in range(1,n+1):
-        if a == b :
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
             graph[a][b] = 0
 
 for _ in range(m):
-    a, b = map(int,input().split())
+    a, b = map(int, input().split())
     graph[a][b] = 1
     graph[b][a] = 1
 
-x, k = map(int,input().split())
+x, k = map(int, input().split())
 
 # 점화식에 따라 플로이드 워셜 알고리즘 수행
-for k in raneg(1,n+1):
-    for i in range(1, n+1):
-        for j in range(1, n+1):
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
             graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
 
-
 distance = graph[1][k] + graph[x][k]
-
-
