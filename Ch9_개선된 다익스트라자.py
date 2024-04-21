361049160ca9a05da1e0e05b5a9390771384ea9d
2taezeat <xorudfl772@naver.com>
2024-04-21 14:46:27 +0900

    [Update] Ch9_개선된 다익스트라자.py

--- Ch9_개선된 다익스트라자.py
+++ Ch9_개선된 다익스트라자.py
@@ -1,39 +1,28 @@
-import heapq
-import sys
-input = sys.stdin.readline
-INF = int(1e9)
+from heapq import *
 
-n,m = map(int,input().split())
+n, m = map(int, input().split())
+INF = int(1e9)
 start = int(input())
-graph = [[] for i in range(n+1)]
-# visited = [False] * (n+1)
-distance = [INF] * (n+1)
+graph = [[] for i in range(n)]
+distance = [float('inf') for _ in range(n)]
 
 for _ in range(m):
-    a,b,c = map(int,input().split())
-    graph[a].append((b,c))
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
 
-
-def dijkstra(start):
+def dijkstra(startNode):
     q = []
-    # 시작 노드로 가기 위한 최단 경로는 0으로 설정하여, 큐에 삽입
-    heapq.heappush(q, (0,start))
-    distance[start] = 0
+    heappush(q, (0, startNode))
+    distance[startNode] = 0
 
-    while q : # 큐가 비어 있지 않다면
-        # 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
-        dist, now = heapq.heappop(q)
+    while q:  # 큐가 비어 있지 않다면
+        curCost, node = heappop(q)
 
-        # 현재 노드가 이미 처리된 적이 있느 노드라면 무시
-        if distance[now] < dist:
+        if distance[node] < curCost:  # 현재 노드가 이미 처리된 적이 있느 노드라면 무시
             continue
 
-        # 현재 노드와 연결된 다른 인접한 노드들을 확인
-        for i in graph[now]:
-            cost = dist + i[1]
-            # 현재 노들를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
-
-            if cost < distance[i[0]]:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
-                
+        for nxt, weight in graph[node]:
+            nCost = curCost + weight
+            if nCost < distance[nxt]:
+                distance[nxt] = nCost
+                heappush(q, (nCost, nxt))
