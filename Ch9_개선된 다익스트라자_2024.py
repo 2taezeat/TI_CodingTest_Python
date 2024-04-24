fad2ebc19e2c3b564bad54fa96401a6ffb1917a3
2taezeat <xorudfl772@naver.com>
2024-04-24 14:22:16 +0900

    [update] Ch9_개선된 다익스트라자_2024.py

--- Ch9_개선된 다익스트라자_2024.py
+++ Ch9_개선된 다익스트라자_2024.py
@@ -18,11 +18,11 @@ def dijkstra(startNode):
     while q:  # 큐가 비어 있지 않다면
         curCost, node = heappop(q)
 
-        if distance[node] < curCost:  # 현재 노드가 이미 처리된 적이 있느 노드라면 무시
+        if distance[node] < curCost:  # 만약 지금 꺼낸 것보다 더 짧은 경로를 알고 있다면 지금 꺼낸 것을 무시
             continue
 
         for nxt, weight in graph[node]:
             nCost = curCost + weight
-            if nCost < distance[nxt]:
+            if nCost < distance[nxt]: # 더 짧은 경로를 발견하면, dist를 갱신하고 우선수위 큐에 넣는다.
                 distance[nxt] = nCost
                 heappush(q, (nCost, nxt))
